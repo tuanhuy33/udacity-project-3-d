4fd166c464119a9a4ce618d62ac84ff07ffbcd6e
tuanhuy33 <tuanhuy6933@gmail.com>
2024-05-05 19:51:10 +0700

    update code

---
+++ set_env.bat
@@ -0,0 +1,11 @@
+@echo off
+
+setx POSTGRES_USERNAME  "postgres"
+setx POSTGRES_PASSWORD  "anh_vip2014"
+setx POSTGRES_HOST  "udacity-instance.ch6aqzxndogx.us-east-1.rds.amazonaws.com"
+setx POSTGRES_DB  "postgres"
+setx AWS_BUCKET  "udacity-instance"
+setx AWS_REGION  "us-east-1"
+setx AWS_PROFILE  "default"
+setx JWT_SECRET "testing"
+setx URL  "http://localhost:8100"
