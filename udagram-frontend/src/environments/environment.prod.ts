114182b7882ca2388d3fb23ed840cb3817838a8b
tuanhuy33 <tuanhuy6933@gmail.com>
2024-05-11 17:40:48 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a748b9a4b93f94341a02286e98be2a27-807974643.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
