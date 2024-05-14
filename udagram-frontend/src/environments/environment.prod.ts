0646c0afcbc8aaa8a91fbdc4d786a4eae4ce9d52
tuanhuy33 <tuanhuy6933@gmail.com>
2024-05-14 16:05:30 +0700

    update apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0a8caeaca9ad42de8c6861e720b5309-153981735.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
